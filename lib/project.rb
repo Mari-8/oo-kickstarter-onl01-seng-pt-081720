700f004c11d819b9fb30969da7ad542fa9d5ec78
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-13 15:45:26 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -3,9 +3,11 @@ require 'pry'
 class Project 
   attr_reader :title, :backers
   
+   @backers = []
+   
   def initialize(title)
     @title = title 
-    @backers = []
+   
   end
   
   def add_backer(backer)

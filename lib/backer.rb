ca1686708ba6983f8b3a8492072e3ecf01491949
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-13 15:45:03 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,7 +11,7 @@ class Backer
   
   def back_project(project)
     @backed_projects = [project]
-    project = Project.backers 
+    project = Project.all
     project.add_backer(self)
   end
 end 

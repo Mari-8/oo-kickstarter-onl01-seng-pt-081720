010db2e7998fd17fea0680d4f3c0499d48cab3ec
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-13 14:56:19 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,7 +4,7 @@ class Backer
   attr_accessor :name
   attr_reader :backed_projects 
   
-  def initialize(name, backed_projects)
+  def initialize(name)
     @name = name
     @backed_projects = []
   end
